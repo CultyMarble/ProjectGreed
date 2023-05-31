2176319292bcf3715992fd291aebed38a8699b49
Tin Ly <lytranhoantin811@gmail.com>
2023-05-31 12:31:10 -0700

    Fix player Currencies is not displayed correctly

--- ProjectGreedFallenKingdom/Assets/Scripts/MainCanvas/GameplayInfoUI/DisplayPlayerCurrency.cs
+++ ProjectGreedFallenKingdom/Assets/Scripts/MainCanvas/GameplayInfoUI/DisplayPlayerCurrency.cs
@@ -3,57 +3,17 @@ using TMPro;
 
 public class DisplayPlayerCurrency : SingletonMonobehaviour<DisplayPlayerCurrency>
 {
-    [SerializeField] private GameObject player;
     [SerializeField] private TextMeshProUGUI tempCurrencyText;
     [SerializeField] private TextMeshProUGUI permCurrencyText;
 
     //===========================================================================
-    private void OnEnable()
+    public void UpdateTempCurrencyText(int amount)
     {
-        EventManager.BeforeSceneUnloadEvent += EventManager_BeforeSceneUnloadEventHandler;
-
-        EventManager.AfterSceneLoadEvent += EventManager_AfterSceneLoadEventHandler;
-
-        player.GetComponent<PlayerCurrencies>().OnTempCurrencyAmountChanged += Instance_OnTempCurrencyAmountChangedHandler;
-        player.GetComponent<PlayerCurrencies>().OnPermCurrencyAmountChanged += Instance_OnPermCurrencyAmountChangedHandler;
+        tempCurrencyText.SetText(amount.ToString());
     }
 
-    private void OnDisable()
+    public void UpdatePermCurrencyText(int amount)
     {
-        EventManager.BeforeSceneUnloadEvent -= EventManager_BeforeSceneUnloadEventHandler;
-
-        EventManager.AfterSceneLoadEvent -= EventManager_AfterSceneLoadEventHandler;
-    }
-
-    //===========================================================================
-    private void EventManager_BeforeSceneUnloadEventHandler()
-    {
-        return;
-    }
-
-    private void EventManager_AfterSceneLoadEventHandler()
-    {
-        UpdateTempCurrencyText();
-        UpdatePermCurrencyText();
-    }
-
-    private void Instance_OnTempCurrencyAmountChangedHandler(object sender, PlayerCurrencies.OnTempCurrencyAmountChangedEventArgs e)
-    {
-        UpdateTempCurrencyText();
-    }
-
-    private void Instance_OnPermCurrencyAmountChangedHandler(object sender, PlayerCurrencies.OnPermCurrencyAmountChangedEventArgs e)
-    {
-        UpdatePermCurrencyText();
-    }
-
-    public void UpdateTempCurrencyText()
-    {
-        tempCurrencyText.SetText(player.GetComponent<PlayerCurrencies>().GetTempCurrencyAmount().ToString());
-    }
-
-    public void UpdatePermCurrencyText()
-    {
-        permCurrencyText.SetText(player.GetComponent<PlayerCurrencies>().GetPermCurrencyAmount().ToString());
+        permCurrencyText.SetText(amount.ToString());
     }
 }
