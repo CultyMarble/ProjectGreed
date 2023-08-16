0c432f52808e8def4200d99ad9d7d1dcf7ef0039
Tin Ly <lytranhoantin811@gmail.com>
2023-08-16 13:53:34 -0700

    Adjust some tilemap

--- Assets/Scripts/Entities/Player/CoreAbility/BombAbility.cs
+++ Assets/Scripts/Entities/Player/CoreAbility/BombAbility.cs
@@ -95,6 +95,7 @@ public class BombAbility : MonoBehaviour
                 bomb.gameObject.SetActive(true);
 
                 currentCharge--;
+                Debug.Log(currentCharge);
 
                 break;
             }
