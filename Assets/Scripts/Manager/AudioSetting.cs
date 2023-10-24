9ead33aa8939a557a72fe24c67da26b23510c3e1
david0617 <davidyu18800@gmail.com>
2023-10-24 15:24:15 -0700

    AudioSetting

---
+++ Assets/Scripts/Manager/AudioSetting.cs
@@ -0,0 +1,97 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class AudioSetting : SingletonMonobehaviour<AudioSetting>
+{
+    [SerializeField] private AudioMixer myMixer;
+    [SerializeField] private Slider masterSlider;
+    [SerializeField] private Slider musicSlider;
+    [SerializeField] private Slider SFXSlider;
+
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource SFXSource;
+    [SerializeField] private AudioClip[] musicSoundClips;
+    [SerializeField] private AudioClip[] SFXSoundClips;
+
+    private static AudioSetting instance;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void Start()
+    {
+        if (PlayerPrefs.HasKey("masterVolume") || PlayerPrefs.HasKey("musicVolume") || PlayerPrefs.HasKey("SFXVolume"))
+        {
+            LoadVolume();
+        }
+        else
+        {
+            setMasterVolume();
+            setMusicVolume();
+            setSFXVolume();
+        }
+    }
+    public void setMasterVolume()
+    {
+        float volume = masterSlider.value;
+        myMixer.SetFloat("Master", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("masterVolume", volume);
+    }
+    public void setMusicVolume()
+    {
+        float volume = musicSlider.value;
+        myMixer.SetFloat("Music", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
+    }
+    public void setSFXVolume()
+    {
+        float volume = SFXSlider.value;
+        myMixer.SetFloat("SFX", Mathf.Log10(volume) * 20);
+        PlayerPrefs.SetFloat("SFXVolume", volume);
+    }
+    private void LoadVolume()
+    {
+        if (PlayerPrefs.HasKey("masterVolume"))
+        {
+            masterSlider.value = PlayerPrefs.GetFloat("masterVolume");
+            setMasterVolume();
+        }
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+            setMusicVolume();
+        }
+        if (PlayerPrefs.HasKey("SFXVolume"))
+        {
+            SFXSlider.value = PlayerPrefs.GetFloat("SFXVolume");
+            setSFXVolume();
+        }
+    }
+    public void playMusicClip(int index)
+    {
+        if (index - 1 >= 0)
+        {
+            musicSource.clip = musicSoundClips[index - 1];
+            musicSource.Play();
+        }
+    }
+    public void playSFXClip(int index)
+    {
+        if (index - 1 >= 0)
+        {
+            SFXSource.clip = SFXSoundClips[index - 1];
+            SFXSource.Play();
+        }
+    }
+}
