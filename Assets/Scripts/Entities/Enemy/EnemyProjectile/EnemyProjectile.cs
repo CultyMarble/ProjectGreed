17bb0980f34f0329e0d54cceb7d15a54e7382b2a
Taylor Daviss <89953361+twdaviss@users.noreply.github.com>
2023-11-05 20:11:17 -0800

    Spawn Points removed from Room centers
    Removed spawn points from the center of rooms to prevent items overlapping with eachother
    Enemy projectiles now look line players ranged projectiles but red

--- Assets/Scripts/Entities/Enemy/EnemyProjectile/EnemyProjectile.cs
+++ Assets/Scripts/Entities/Enemy/EnemyProjectile/EnemyProjectile.cs
@@ -11,6 +11,14 @@ public class EnemyProjectile : MonoBehaviour
     private Vector3 moveDirection;
     private float lifeTime = 5f;
 
+    [Header("Effect Animation Settings:")]
+    [SerializeField] private SpriteRenderer abilityEffect;
+    [SerializeField] private Sprite[] effectSprites;
+
+    private readonly float animationSpeed = 0.1f;
+    private float animationTimer;
+    private int currentAnimationIndex;
+
     //===========================================================================
     private void OnCollisionEnter2D(Collision2D collision)
     {
@@ -43,8 +51,22 @@ public class EnemyProjectile : MonoBehaviour
         {
             Despawn();
         }
+        ProjectileAnimation();
     }
+    private void ProjectileAnimation()
+    {
+        animationTimer += Time.deltaTime;
+        if (animationTimer >= animationSpeed)
+        {
+            animationTimer -= animationSpeed;
 
+            if (currentAnimationIndex == effectSprites.Length)
+                currentAnimationIndex = 0;
+
+            abilityEffect.sprite = effectSprites[currentAnimationIndex];
+            currentAnimationIndex++;
+        }
+    }
     private void Despawn()
     {
         lifeTime = 5;
@@ -61,5 +83,6 @@ public class EnemyProjectile : MonoBehaviour
     {
         moveDirection = newMoveDirection;
         moveSpeed = newSpeed;
+        transform.up = moveDirection;
     }
 }
