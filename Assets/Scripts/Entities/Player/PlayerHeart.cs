06dd311e5ebec67cf721b437161af3ce78b57a40
Tin Ly <lytranhoantin811@gmail.com>
2023-11-28 13:58:47 -0800

    Revert "Manual Solve Conflict"
    This reverts commit 8ed070e4d3d16d9a94385d95eadde865111a5544.

--- Assets/Scripts/Entities/Player/PlayerHeart.cs
+++ Assets/Scripts/Entities/Player/PlayerHeart.cs
@@ -19,35 +19,29 @@ public class PlayerHeart : MonoBehaviour
     private readonly float feedbackDamageTime = 0.10f;
     private float feedbackDamageTimer = default;
 
-    // Player Regen
-    private bool canRegen = default;
-    private readonly float recoveryCooldown = 15.0f;
+    // Player Buff
+    private readonly float recoveryCooldown = 10.0f;
     private float recoveryCooldownTimer = default;
-
-    // IFrame
-    [SerializeField] private Animator bodySpriteAnimator = default;
-    private readonly float iFrameDuration = 1.0f;
-    private float iFrameTimer = default;
-    private bool damageImmune = default;
 
     //======================================================================
     private void OnEnable()
     {
         ResetPlayerHeart();
-        recoveryCooldownTimer = recoveryCooldown;
     }
 
     private void Update()
     {
         UpdateDamageFeedBackTimer();
-        UpdateIFrameTimer();
 
-        if (canRegen == false)
-            return;
+        if (Input.GetKeyDown(KeyCode.Home))
+        {
+            UpdateCurrentMaxHeart(1);
+        }
 
+        // Recovery
         if (currentHeart < currentMaxHeart)
         {
-            recoveryCooldownTimer -= Time.deltaTime;
+            recoveryCooldownTimer-= Time.deltaTime;
             if (recoveryCooldownTimer <= 0.0f)
             {
                 recoveryCooldownTimer = recoveryCooldown;
@@ -57,33 +51,11 @@ public class PlayerHeart : MonoBehaviour
     }
 
     //======================================================================
-    private void IFrameActive()
-    {
-        iFrameTimer = iFrameDuration;
-        bodySpriteAnimator.SetBool("IFrame", true);
-
-        damageImmune = true;
-    }
-
-    private void UpdateIFrameTimer()
-    {
-        if (iFrameTimer <= 0.0f)
-            return;
-
-        iFrameTimer -= Time.deltaTime;
-        if (iFrameTimer <= 0.0f)
-        {
-            bodySpriteAnimator.SetBool("IFrame", false);
-            damageImmune = false;
-        }
-    }
-
     private void TriggerDamageFeedBack()
     {
         // Health Feedback
         GetComponentInChildren<SpriteRenderer>().color = new Color(255, 0, 0);
         feedbackDamageTimer = feedbackDamageTime;
-
         AudioManager.Instance.playSFXClip(AudioManager.SFXSound.playerDamage);
     }
 
@@ -113,11 +85,6 @@ public class PlayerHeart : MonoBehaviour
         }
 
     //======================================================================
-    public void SetCanRegenActive(bool active)
-    {
-        canRegen = active;
-    }
-
     public void ResetPlayerHeart()
     {
         currentHeart = currentMaxHeart;
@@ -146,36 +113,34 @@ public class PlayerHeart : MonoBehaviour
         //Invoke Event
         OnHeartChangedEvent?.Invoke(this, new OnHealthChangedEventArgs { currentHeart = currentHeart });
     }
-
     public void UpdateCurrentHeart(int amount = 0)
     {
-        if (amount < 0 && damageImmune == true)
+        if (!this.enabled)
+        {
             return;
-
+        }
         if (amount < 0)
-        {
             TriggerDamageFeedBack();
-            IFrameActive();
-        }
 
-        currentHeart += amount;
-
-        if (currentHeart <= 0)
+        if (amount != 0)
         {
-            currentHeart = 0;
-            DespawnPlayer();
 
-            recoveryCooldownTimer = recoveryCooldown;
+            feedbackDamageTimer = feedbackDamageTime;
+
+            currentHeart += amount;
+            if (currentHeart <= 0)
+            {
+                currentHeart = 0;
+                DespawnPlayer();
+            }
+            else if (currentHeart > currentMaxHeart)
+            {
+                currentHeart = currentMaxHeart;
+            }
         }
-        else if (currentHeart > currentMaxHeart)
-            currentHeart = currentMaxHeart;
-
-        feedbackDamageTimer = feedbackDamageTime;
-
         //Invoke Event
         OnHeartChangedEvent?.Invoke(this, new OnHealthChangedEventArgs { currentHeart = currentHeart });
     }
-
     public void UpdatePlayerHeartParameters()
     {
         currentMaxHeart = PlayerDataManager.Instance.PlayerDataRuntime.BaseMaxHealth;
